dae9f3f419139b0d0dd188e6f8f31e6f9031c026
xavierdf <xavierdf@protonmail.ch>
2017-12-07 12:32:43 +0000

    Adiniora inversor de maiusculas/minusculas

---
+++ Desktop/inverte.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define MAXSTR 500
+int main() {
+    char str[MAXSTR];
+    char diff = 'a' - 'A';
+    while (fgets(str, MAXSTR, stdin) != NULL) {
+        for (int i = 0; str[i] != '\0'; ++i) {
+            if ((str[i] >= 'A') && (str[i] <= 'Z'))
+                printf("%c", str[i] + diff);
+            else if ((str[i] >= 'A') && (str[i] <= 'Z'))
+                printf("%c", str[i] + diff);
+            else
+                printf("%c", str[i]);
+        }
+    }
+	
+	printf("Adeus!\n");
+	return 0;
+
+}
